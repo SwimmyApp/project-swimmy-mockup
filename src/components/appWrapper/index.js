8914c0217a98b4ec4f76b4149b8c41d87861564c
Florida Elago <florida.elago@casper.com>
2017-01-14 20:28:18 -0500

    adds back button functionality

--- src/components/appWrapper/index.js
+++ src/components/appWrapper/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import styles from './style.css';
 
 class AppWrapper extends React.Component {
+  goBack() {
+    window.history.back()
+  }
   render () {
     return (
       <div className={styles.container}>
         <header>
-          <button onClick={}>back</button>
+          <button onClick={this.goBack.bind(this)}>back</button>
           <h1 className={styles.titleText}>{this.props.title}</h1>
         </header>
         {this.props.children}
