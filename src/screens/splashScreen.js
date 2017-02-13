97ca5316fa1265ef33e671b870ed9fc0e5ed6d43
Julia Nguyen <julia@8thsq.co>
2017-02-12 20:55:40 -0500

    Modified existing splash screen to match mock up

--- src/screens/splashScreen.js
+++ src/screens/splashScreen.js
@@ -2,34 +2,47 @@ import React from 'react';
 import {observer} from 'mobx-react';
 import views from '../config/views';
 import AppWrapper from '../components/appWrapper'
-import styles from './styles/splashScreenStyles.scss';
+import * as styles from './styles/splashScreenStyles.scss';
 
 class SplashScreen extends React.Component {
   render() {
     const { store } = this.props;
     const {router: {goTo}} = store;
-
     return (
       <AppWrapper title='Swimmy'>
         <div className={styles.logoPlaceholder}>
-          <p>Logo Placeholder</p>
+          <div className={styles.fishies} />
         </div>
-
+        <div className={styles.content}>
+          <div>
+            <h3>Project Swimmy</h3>
+            <ol>
+              <li>Meet local organizing activists</li>
+              <li>Unite for collective action</li>
+              <li>Win for progressive change</li>
+            </ol>
+          </div>
+        </div>
         <div className={styles.buttonContainer}>
           <button
             className={styles.buttons}
-            onClick={() => {goTo(views.login)}}>I'm a new user</button>
-
+            onClick={() => {goTo(views.login)}}>I'm a new volunteer</button>
           <button
             className={styles.buttons}
             onClick={() => {goTo(views.login)}}>I'm an organization</button>
         </div>
-        <div>
-          <p>Already have an account? <a href='#' onClick={() => {goTo(views.login)}}>Sign in</a></p>
-        </div>
+        <nav className={"navbar-fixed-bottom " + styles.footer}>
+          <div className="container">
+            <p>Already have an account? 
+            <a href='#' onClick={() => {goTo(views.login)}}> Sign in!</a>
+            </p>
+          </div>
+        </nav>
       </AppWrapper>
     )
   }
 }
 
 export default observer(['store'], SplashScreen);
+
+// TODO: Footer nav should be turned into a component once design is finalized.
