a57a94cb638fde7228a821eb09d35dc110a73c34
Julia Nguyen <julia@8thsq.co>
2017-02-18 15:42:54 -0500

    Edit routes to direct to proper screen - Text and CSS changes

--- src/screens/signupScreen.js
+++ src/screens/signupScreen.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import {observer} from 'mobx-react';
 import views from '../config/views';
-import * as styles from './styles/loginScreen.scss';
+import * as styles from './styles/signupScreens.scss';
 import AppWrapper from '../components/appWrapper'
 
 const css = {
-  form: {
-    fontFamily: 'Montserrat, sans-serif',
-    marginTop: '50%',
-    textAlign: 'center'
-  },
   facebook: {
     backgroundColor: '#388CF3',
     fontSize: '18px',
@@ -40,42 +35,54 @@ class SignupScreen extends React.Component {
     const {store} = this.props;
     const {router: {goTo}} = store;
     return (
-      <AppWrapper title='Welcome!' backButton>
+      <AppWrapper title='Welcome Activist!' backButton>
         <div className={styles.container}>
         <form className='center-block' onSubmit={this.handleOnSubmit.bind(this)} method='post'>
-          <input
-            type='button'
-            className={styles.buttons}
-            style={css.facebook}
-            value='Connect with Facebook'
-          />
-          <input
-            type='text'
-            placeholder='Enter your name'
-          />
-          <input
-            type='email'
-            placeholder='Enter your email address'
-          />
-          <input
-            type='password'
-            placeholder='Enter your Pa$$word123'
-          />
-          <input
-            type='text'
-            placeholder='Enter your phone number (Optional)'
-          />
+          <div className='form-group'>
+            <input
+              className={styles.input}
+              type='text'
+              placeholder='Enter your name'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              className={styles.input}
+              type='email'
+              placeholder='Enter your email address'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              className={styles.input}
+              type='password'
+              placeholder='Enter your Pa$$word123'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              className={styles.input}
+              type='text'
+              placeholder='Enter your phone number (Optional)'
+            />
+          </div>
           <input
             type='checkbox'
             id='terms-conditions'
           />
           <label htmlFor='terms-conditions'>I agree to <a href='#' target='_blank'>terms and conditions</a></label>
-          <input className={styles.buttons} style={css.submit} type='submit' value='Sign In'/>
+          <input className={styles.buttons} style={css.submit} type='submit' value='Sign Up'/>
+          <input
+              type='button'
+              className={styles.buttons}
+              style={css.facebook}
+              value='Connect with Facebook'
+          />
         </form>
         </div>
         <nav className="navbar-fixed-bottom" style={css.footer}>
           <div className="container">
-            <p>Already have an account? <a href='#' onClick={() => {goTo(views.login)}}>Sign in!</a></p>
+            <p>Already have an account? <a href='#' onClick={() => {goTo(views.login)}}>Log in!</a></p>
           </div>
         </nav>
       </AppWrapper>
